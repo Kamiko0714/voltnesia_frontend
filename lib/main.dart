22c23c543460949473e8a476a8b2d5aa9597fd35
Kamiko0714 <dewaathallah2003@gmail.com>
2024-12-10 21:55:20 +0700

    lupa njir

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-// import 'login/firstpage.dart';
-import 'voltnesia2k24/homepage.dart';
+import 'login/firstpage.dart';
+// import 'voltnesia2k24/homepage.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      home: Firstpage(),
     );
   }
 }
